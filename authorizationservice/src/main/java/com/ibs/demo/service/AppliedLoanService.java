c56c6997664a3e7d3d83411a9cc2b7ccfdc733c3
PrashikGaikwad28 <84444580+PrashikGaikwad28@users.noreply.github.com>
2023-02-16 11:51:23 +0530

    Restricting loan amount changes made

--- authorizationservice/src/main/java/com/ibs/demo/service/AppliedLoanService.java
+++ authorizationservice/src/main/java/com/ibs/demo/service/AppliedLoanService.java
@@ -31,7 +31,20 @@ public class AppliedLoanService {
 	public AppliedLoans applyLoan(String userName, AppliedLoans appliedLoans) {
 		User user = userService.findByUserName(userName);
 		appliedLoans.setUser_id(user);
-		return appliedLoanRepository.save(appliedLoans);
+//		AppliedLoanService service = new AppliedLoanService();		//changes
+		List<AppliedLoans> loans = appliedLoanRepository.findLoansByUser(user.getId());
+		long totalEmi = appliedLoans.getMonthly_emi();
+		Iterator<AppliedLoans> iterator = loans.listIterator();
+		while(iterator.hasNext()) {
+			totalEmi += iterator.next().getMonthly_emi();
+		}
+		System.out.println("\nTotal EMI: "+totalEmi);				
+		long monthlySalary = appliedLoans.getAnnual_income()/12;	
+		if(totalEmi <= monthlySalary*2/3) {
+			return appliedLoanRepository.save(appliedLoans);
+		}else {
+			throw new InvalidRequestException("Loan amount exceeds the maximum limit!! Please enter valid amount.");
+		}								//end of changes
 	}
 	
 	public List<AppliedLoans> getAllLoanDetails(){
